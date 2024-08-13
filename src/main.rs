703bc9ebfc365f710e009db2af3c86e2512e923e
BabakMahmoodi <babakthebest@gmail.com>
2024-08-13 14:04:25 +0330

    stable twosum

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ fn main() {
     // let arr = [2, 8, 5, 4 ,3, 6];
      let arr: Vec<i32> = (1..=10000).collect();
     let target=19999;
-let two_sum_v1 =two_sum(&arr,target);
-let two_sum_v2=two_sum_v2(&arr,target);
+// let two_sum_v1 =two_sum(&arr,target);
+// let two_sum_v2=two_sum_v2(&arr,target);
 measure_performance(|| two_sum(&arr, target), "two_sum_v1");
-// println!("arr is :{:?}",arr);
-println!("two_sum_v1 is :{:?}",two_sum_v1);
-println!("two_sum_v2 is :{:?}",two_sum_v2);
+measure_performance(|| two_sum_v2(&arr, target), "two_sum_v2");
+
+// // println!("arr is :{:?}",arr);
+// println!("two_sum_v1 is :{:?}",two_sum_v1);
+// println!("two_sum_v2 is :{:?}",two_sum_v2);
 
 }
